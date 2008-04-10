6dc485f1d221412230d9d3994fde3a93e3c46585
sunny <negatif@gmail.com>
2008-04-10 12:48:06 +0200

    Flashy brand new version can read external csv lists and feed that juicy data in templates

--- wubmail.rb
+++ wubmail.rb
@@ -3,21 +3,36 @@
 # WubMail is a tiny wrapper to send emails with an erb template.
 # by Sunny Ripert
 #
-# Example
-# --------
-# welcome.txt:
-#    From: Sunny <sunny@sunfox.org>
-#    To: <%= to %>
-#    Subject: HAY <%= name %>!1
-#    X-Mailer: LOLMAILOR
-#    <%= headers %>
-#    
-#    Hello <%= name %>,
-#    You are one hell of a smart <%= sex == :female ? "woman" : "guy" %>!
+# Quick Example
+# -------------
+#   WubMail.new(open('welcome.txt').read, :email => "negatif@gmail.com",
+#     :name => "Sunny", :sex => "male").send!
 #
-# send_these_mails_already.rb:
-#    WubMail.new(:welcome, :to => "negatif@gmail.com", :name => "Negatif", :sex => :male).send!
-#    WubMail.new(:welcome, :to => "srzt@hotchicks.com", :name => "Susy Rozet", :sex => :female).send!
+# Full Example
+# -------
+# welcome.txt:
+#   From: Sunny <sunny@sunfox.org>
+#   To: <%= name%> <<%= email %>>
+#   Subject: HAY <%= name %>!1
+#   X-Mailer: Wubmail
+#    
+#   Hello <%= name %>,
+#   You are one hell of a cute <%= sex == "female" ? "girl" : "boy" %>!
+#
+# users.csv:
+#   email,name,sex
+#   negatif@gmail.com,Sunny,male
+#   hima@example.com,Audrey,female
+#
+# $ ruby wubmail.rb welcome.txt users.csv
+#   From: Sunny <sunny@sunfox.org>
+#   To: Audrey <hima@example.com>
+#   Subject: HAY Audrey!1
+#   X-Mailer Wubmail
+#
+#   Hello Audrey,
+#   You are one hell of a cute girl!
+#
 #
 # Licence
 # -------
@@ -25,13 +40,15 @@
 
 require 'erb'
 require 'net/smtp'
+require 'open-uri'
 
 class WubMail
   attr_accessor :options
 
-  def initialize(template_name, options = {})
-    @template_name = template_name
+  def initialize(template, options = {})
+    @template = template
     @options = options
+    parse
   end
 
   # allows template to use "name" instead of "options[:name]"
@@ -41,27 +58,83 @@ class WubMail
 
   # sets attributes smtp_message, headers, from and to
   def parse
-    return unless @smtp_message.nil?
-    
     # fetch contents from erb template
-    contents = open("#{@template_name}.txt").read
-    @smtp_message = ERB.new(contents).result(binding)
+    @smtp_message = ERB.new(@template).result(binding)
 
     # build a headers hash
     header_lines = @smtp_message.split("\n\n", 2).first.split("\n")
-    headers_options = header_lines.map { |line| line.split(/: ?/, 2) }
-    @headers = Hash[*header_options.flatten]
+    headers_options = header_lines.collect { |line| line.split(/: ?/, 2) }
+    @headers = Hash[*headers_options.flatten]
 
     # guess emails from headers
-    @from = @headers['From'].gsub(/[<>]/, '').gsub(/^.* /, '').chomp
-    @to = @headers['To'].gsub(/[<>]/, '').gsub(/^.* /, '').chomp
+    @from = header 'From'
+    @to = header 'To'
+  end
+
+  def header(name)
+    @headers[name].gsub(/[<>]/, '').gsub(/^.* /, '').chomp
+  end
+  
+  def to_s
+    @smtp_message
   end
 
   def send!
-    parse
     Net::SMTP.start('localhost') do |smtp|
       smtp.send_message @smtp_message, @from, @to
     end
     true
   end
 end
+
+
+class WubMailCSV
+  attr_reader :recipients
+
+  def initialize(template, csv)
+    @template = open(template).read
+    @csv = open(csv).read
+    parse
+  end
+  
+  def parse
+    # ignore blank lines and comments
+    head, *lines = @csv.gsub(/#.*$/, '').grep(/\w/)
+    headers = head.split(/,/).collect { |h| h.strip.to_sym }
+    
+    @recipients = lines.collect do |line|
+      values = line.split(/,/).collect { |v| v.strip }
+      Hash[*headers.zip(values).flatten]
+    end
+  end
+  
+  def example
+    recipient = @recipients[rand(@recipients.size)]
+    WubMail.new(@template, recipient).to_s
+  end
+  
+  def send!
+    @recipients.each do |recipient_hash|
+      WubMail.new(@template, recipient_hash).send!
+    end
+  end
+end
+
+
+def main
+  abort "Usage: #{$0} [-s] templatefile peoplecsv" if ARGV.size < 2
+
+  emailer = WubMailCSV.new(ARGV[-2], ARGV[-1])
+
+  if ARGV.first == "-s"
+    emailer.send!
+  else
+    puts emailer.example
+    puts
+    puts "Use -s to send it right now to #{emailer.recipients.size} recipients"
+  end
+end
+
+
+main if __FILE__ == $0
+
